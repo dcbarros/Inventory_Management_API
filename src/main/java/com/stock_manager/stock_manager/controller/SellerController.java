9afc9c5a70b25d93454ecb9b4e78d5f4644c8ded
dcbarros <dcbarros1@gmail.com>
2024-09-01 12:14:47 -0300

    feat: Implementa os endpoints da classe de vendedor

--- src/main/java/com/stock_manager/stock_manager/controller/SellerController.java
+++ src/main/java/com/stock_manager/stock_manager/controller/SellerController.java
@@ -5,11 +5,22 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stock_manager.stock_manager.dto.request.SellerCreateDtoRequest;
+import com.stock_manager.stock_manager.dto.request.SellerUpdateDtoRequest;
+import com.stock_manager.stock_manager.dto.response.SellerCreatedResponse;
+import com.stock_manager.stock_manager.dto.response.SellerDetailsDtoResponse;
 import com.stock_manager.stock_manager.service.SellerService;
+
+import jakarta.validation.Valid;
+
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -24,10 +35,26 @@ public class SellerController {
         this.sellerService = sellerService;
     }
 
-    @PostMapping("")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void newSeller(@RequestBody SellerCreateDtoRequest request) {
-        this.sellerService.createNewSeller(request);
+    @PostMapping("/sign-in")
+    @ResponseStatus(HttpStatus.CREATED)
+    public SellerCreatedResponse newSeller(@RequestBody @Valid SellerCreateDtoRequest request) {
+        return this.sellerService.createNewSeller(request);
     }
-        
+
+    @GetMapping("/{uuid}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public SellerDetailsDtoResponse getSellerByUuid(@PathVariable UUID uuid) {
+        return sellerService.getSellerByUuid(uuid);
+    }
+
+    @PutMapping("/{uuid}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateSellerByUuid(@PathVariable UUID uuid, @RequestBody SellerUpdateDtoRequest request) {
+        sellerService.updateSellerByUuid(uuid, request);
+    }    
+
+    @DeleteMapping("/{uuid}")
+    public void deleteSellerByUuid(@PathVariable UUID uuid) {
+        sellerService.deactivateSellerByUuid(uuid);
+    }    
 }
